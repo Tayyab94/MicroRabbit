9e1c7992a7dc350f93da1250199288756354ad16
tayyab bhatti <tayyab.bhatti30@hotmail.com>
2020-04-14 17:31:23 +0500

    Implemented Interfaces and Classes For Domain.core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,22 @@
+﻿//using MicroRabbit.Domain.Core.Event;
+using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Commands
+{
+   public abstract class Command : Message
+    {
+
+        public Command()
+        {
+            TimeStemp = DateTime.Now;
+        }
+
+
+        public DateTime TimeStemp { get; protected set; }
+
+
+    }
+}
